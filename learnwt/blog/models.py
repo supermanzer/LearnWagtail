9319023bcc3483f445b830bd3e03a44a8e62ae9d
Ryan <ryan.manzer@gmail.com>
2019-07-13 10:24:02 -0700

    Added orderables for blog authors and snippets

--- learnwt/blog/models.py
+++ learnwt/blog/models.py
@@ -7,18 +7,35 @@ This models.py file defines the data models for our Blog app.
 
 from django.db import models
 from django.shortcuts import render
+from modelcluster.fields import ParentalKey
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import StreamField, RichTextField
 from wagtail.admin.edit_handlers import (
-    FieldPanel, StreamFieldPanel, RichTextFieldPanel
+    FieldPanel, StreamFieldPanel, RichTextFieldPanel, MultiFieldPanel,
+    InlinePanel
 )
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.models import register_snippet
 
 from streams import blocks
 
+
 # Create your models here.
+class BlogAuthorsOrderable(Orderable):
+    """Provide ability to assign one or more blog authors in a specific order."""
+
+    page = ParentalKey("blog.BlogDetailPage", related_name="blog_authors")
+    author = models.ForeignKey(
+        'blog.BlogAuthor',
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+        SnippetChooserPanel('author')
+    ]
 
 
 class BlogListingPage(RoutablePageMixin, Page):
@@ -94,5 +111,58 @@ class BlogDetailPage(Page):
         FieldPanel('custom_title'),
         RichTextFieldPanel('blog_summary'),
         ImageChooserPanel('blog_image'),
-        StreamFieldPanel('content')
+        StreamFieldPanel('content'),
+        MultiFieldPanel([
+            InlinePanel('blog_authors', label="Author",
+                        min_num=1, max_num=8)
+        ], heading="Blog Authors")
     ]
+
+@register_snippet
+class BlogAuthor(models.Model):
+    """Define a reusable data model for assigning blog authors."""
+    # Snippets are a great way to store and make use of reusable data.
+    # This includes things like categories, menus, etc.
+    # Here we use it to associate blogs with authors.
+    first_name = models.CharField(
+        max_length=100, null=True, blank=False,
+        help_text="Author's first name"
+    )
+    last_name = models.CharField(
+        max_length=120, null=True, blank=False,
+        help_text="Author's last name"
+    )
+    website = models.URLField(blank=True, null=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True, blank=False,
+        related_name='+'
+    )
+
+    def name(self, name_format='f l'):
+        names = {
+            'f l': f'{self.first_name} {self.last_name}',
+            'l f': f'{self.last_name}, {self.first_name}'
+        }
+        return names.get(name_format)
+
+    panels = [
+        MultiFieldPanel([
+            FieldPanel('first_name'),
+            FieldPanel('last_name'),
+            ImageChooserPanel('image'),
+        ], heading='Name and Image'),
+        MultiFieldPanel([
+            FieldPanel('website')
+        ], heading="Links")
+
+    ]
+
+    def __str__(self):
+        """String representative of this class."""
+        return self.name()
+
+    # class Meta:
+    #     verbose_name = 'Blog Author',
+    #     verbose_name_plural = "Blog Authors"
